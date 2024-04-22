8bf4b41cc0c2009b614c4cd9e03fd92aedf5517b
Talkasi <kitalkasi@gmail.com>
2024-04-22 15:39:30 +0300

    CHORE: Setting style corrector

---
+++ style.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+find . -name "*.h" -exec clang-format -i {} \;
+find . -name "*.c" -exec clang-format -i {} \;
+
+find . -name "*.hpp" -exec clang-format -i {} \;
+find . -name "*.cpp" -exec clang-format -i {} \;
