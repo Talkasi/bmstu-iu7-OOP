91faf27a80aea9c3812ecc3017a9bfc28d208db4
Talkasi <kitalkasi@gmail.com>
2024-06-08 22:06:57 +0300

    FEAT: Config updated

---
+++ lab_04_with_2points/config.hpp
@@ -0,0 +1,36 @@
+#ifndef LAB_04_CONFIG_HPP
+#define LAB_04_CONFIG_HPP
+
+enum cabin_id_t
+{
+    FIRST_CABIN_ID,
+    SECOND_CABIN_ID
+};
+
+enum direction_t
+{
+    DOWN = -1,
+    STAND = 0,
+    UP = 1
+};
+
+enum direction_id_t
+{
+    UP_ID,
+    DOWN_ID
+};
+
+#define N_DIRECTIONS 2
+
+#define COLOR_BUTTON_ACTIVE "green"
+#define COLOR_BUTTON_INACTIVE "grey"
+
+#define N_FLOORS 10
+#define START_FLOOR 1
+#define N_CABINS 2
+
+#define WAIT_TIME 1000
+#define DOOR_TIME 1000
+#define MOVE_TIME 1000
+
+#endif // LAB_04_CONFIG_HPP
